6ba44f8f36c340b2e045d9e0de8c9829c23dc800
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2024-10-04 12:01:19 -0700

    refac(main): boiler plate entry form

---
+++ src/app/ui/entry-form.tsx
@@ -0,0 +1,18 @@
+export default function Form() {
+  return (
+    <form>
+      <div>
+        <label>Flags</label>
+        <textarea></textarea>
+      </div>
+      <div>
+        <label>Background Color</label>
+        <input></input>
+      </div>
+      <div>
+        <button>Go!</button>
+      </div>
+    </form>
+  )
+}
+
