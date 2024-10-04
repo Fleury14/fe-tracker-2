72cbd9ab0676512b5908a52bf4d3797063286443
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2024-10-04 13:12:57 -0700

    chore(entry form/tracker): redirect upon submission to boiler plate tracker

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,7 +1,15 @@
 'use server'
 
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
 export async function entrySubmit(formData: FormData) {
+
   const flags = formData.get("flags")
   const bgColor = formData.get("bg-color")
-    console.log(flags, bgColor);
-  }
+  // console.log(flags, bgColor);
+
+  revalidatePath('/tracker');
+  redirect(`/tracker/${flags}/${bgColor}`);
+
+}
