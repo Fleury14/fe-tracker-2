76621034d8080a029e8ed15ea8e9643ac41d3ab1
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2024-10-04 12:27:05 -0700

    feat(entry form): stylize form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import EntryForm from '@/app/ui/entry-form';
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1>FE Web Tracker</h1>
+    <div className="flex flex-col justify-center items-center h-screen font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl m-10">FE Web Tracker</h1>
       <EntryForm />
     </div>
   );
--- src/app/ui/entry-form.tsx
+++ src/app/ui/entry-form.tsx
@@ -1,16 +1,16 @@
 export default function Form() {
   return (
-    <form>
-      <div>
+    <form className="w-full">
+      <div className="flex flex-col m-5 w-full items-center">
         <label>Flags</label>
-        <textarea></textarea>
+        <textarea rows={6} className="min-w-96 w-3/4 text-slate-950"></textarea>
       </div>
-      <div>
+      <div className="flex flex-col m-5 w-full items-center">
         <label>Background Color</label>
         <input></input>
       </div>
-      <div>
-        <button>Go!</button>
+      <div className="flex justify-center m-5 w-full">
+        <button className="bg-slate-700 p-3 rounded hover:bg-slate-600">Go!</button>
       </div>
     </form>
   )
