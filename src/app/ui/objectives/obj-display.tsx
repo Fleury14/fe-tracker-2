223743a8fa4072eecb9bc6156491b624c2e88096
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2024-10-07 14:16:27 -0700

    refac(obj display): edit button

--- src/app/ui/objectives/obj-display.tsx
+++ src/app/ui/objectives/obj-display.tsx
@@ -1,14 +1,18 @@
 import { TObjective } from "@/app/lib/interfaces";
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
 
 export default function ObjectiveDisplay({ objectives, req, onEdit }: { objectives: TObjective[], req: number, onEdit: Function }) {
     return (
         <div className="flex flex-col">
-            <p>Objectives - Required: {req}</p>
+            <div className="flex justify-between">
+                <p className="font-bold">Objectives</p>
+                <p className="font-bold font-[family-name:var(--font-geist-mono)]">Required: {req}</p>
+            </div>
             {objectives.map(obj => {
                 return (
-                    <div key={obj.label} className="flex">
-                        <p>{obj.label}</p>
-                        {obj.random && <button onClick={() => onEdit(obj.id)}>Edit</button>}
+                    <div key={obj.label} className="flex items-center hover:bg-slate-800">
+                        <p className="mr-3">{obj.label}</p>
+                        {obj.random && <button onClick={() => onEdit(obj.id)}><PencilSquareIcon className="size-6 text-yellow-400" /></button>}
                     </div>
                 );
             })}
