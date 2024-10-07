656779f98bb5298826ca5360f802592cb29f9a7d
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2024-10-06 18:14:10 -0700

    feat(timer): button and heroicons

--- src/app/ui/timer/timer-display.tsx
+++ src/app/ui/timer/timer-display.tsx
@@ -1,5 +1,7 @@
 import ParseTime from "@/app/lib/parse-time";
-
+import {
+    PlayIcon, StopIcon, BackspaceIcon
+  } from '@heroicons/react/24/solid';
 export default function TimerDisplay ({
     currentTime,
     startTime
@@ -10,9 +12,9 @@ export default function TimerDisplay ({
     return (
         <div className="flex flex-col h-full justify-end items-center">
             <div className="flex justify-around">
-                <button>Start</button>
-                <button>Stop</button>
-                <button>Reset</button>
+                <button className="m-5 w-12 h-10 border flex justify-center items-center bg-slate-800 hover:bg-slate-600 border-slate-600"><PlayIcon className="size-6"/></button>
+                <button className="m-5 w-12 h-10 border flex justify-center items-center bg-slate-800 hover:bg-slate-600 border-slate-600"><StopIcon className="size-6"/></button>
+                <button className="m-5 w-12 h-10 border flex justify-center items-center bg-slate-800 hover:bg-slate-600 border-slate-600"><BackspaceIcon className="size-6"/></button>
             </div>
             <p className="text-5xl font-bold">{ParseTime(currentTime - startTime)}</p>
         </div>
