2c116410bc584d91604658b3ae3a9d086361fecc
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2024-10-08 12:17:34 -0700

    chore(app): wrapped the whole app in suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import EntryForm from '@/app/ui/entry-form';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl m-10">FE Web Tracker</h1>
-      <EntryForm />
-    </div>
+    <Suspense>
+      <div className="flex flex-col justify-center items-center h-screen font-[family-name:var(--font-geist-sans)]">
+        <h1 className="text-4xl m-10">FE Web Tracker</h1>
+        <EntryForm />
+      </div>
+    </Suspense>
   );
 }
