249cd556dc7852df8c832b62c98e1cf91eca4096
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2024-10-09 10:30:50 -0700

    fix(timer/objectives): give icon buttons invisible text, strengthen objective fonts

--- src/app/ui/timer/timer-display.tsx
+++ src/app/ui/timer/timer-display.tsx
@@ -18,9 +18,9 @@ export default function TimerDisplay ({
     return (
         <div className="flex flex-col h-full justify-end">
             <div className="flex justify-center w-100">
-                <button onClick={() => startTimer()} disabled={isActive} className="m-5 w-12 h-10 border flex justify-center items-center bg-slate-800 hover:bg-slate-600 border-slate-600"><PlayIcon className="size-6"/></button>
-                <button onClick={() => stopTimer()} className="m-5 w-12 h-10 border flex justify-center items-center bg-slate-800 hover:bg-slate-600 border-slate-600"><StopIcon className="size-6"/></button>
-                <button onClick={() => resetTimer()} className="m-5 w-12 h-10 border flex justify-center items-center bg-slate-800 hover:bg-slate-600 border-slate-600"><BackspaceIcon className="size-6"/></button>
+                <button onClick={() => startTimer()} disabled={isActive} className="m-5 w-12 h-10 border flex justify-center items-center bg-slate-800 hover:bg-slate-600 border-slate-600"><p className="invisible w-0 h-0">Start Timer</p><PlayIcon className="size-6"/></button>
+                <button onClick={() => stopTimer()} className="m-5 w-12 h-10 border flex justify-center items-center bg-slate-800 hover:bg-slate-600 border-slate-600"><p className="invisible w-0 h-0">Stop Timer</p><StopIcon className="size-6"/></button>
+                <button onClick={() => resetTimer()} className="m-5 w-12 h-10 border flex justify-center items-center bg-slate-800 hover:bg-slate-600 border-slate-600"><p className="invisible w-0 h-0">Reset Timer</p><BackspaceIcon className="size-6"/></button>
             </div>
             <div className="ml-32">
                 <p className="text-5xl font-bold">{ParseTime(currentTime)}</p>
