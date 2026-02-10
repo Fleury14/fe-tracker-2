a8e90aa2be6aaab9b267569c65afcbc7d2cb8c89
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2026-02-10 15:47:09 -0800

    feat(entryform): included presets

--- src/app/lib/presets.ts
+++ src/app/lib/presets.ts
@@ -46,6 +46,23 @@ const presets:PresetGroup[] = [
                 flags: "OA1:quest_monsterqueen/2:quest_monsterking/3:quest_murasamealtar/4:quest_crystalaltar/5:quest_whitealtar/6:quest_ribbonaltar/7:quest_masamunealtar/8:quest_baronbasement/random:3,quest/do_1:spoon/do_2:spoon/do_3:spoon/do_4:spoon/do_5:spoon/do_6:spoon/do_7:spoon/do_8:spoon/do_9:spoon/do_10:spoon/do_all:spoon OB1:quest_forge/2:quest_tradepan/3:quest_sealedcave/4:quest_tradepink/do_1:spoon/do_2:spoon/do_3:crystal Kmain/summon/moon Pkey Crelaxed/nofree/distinct:7/start:edge/j:abilities/nekkie Tpro/playable/sparse:70/miabs:standard Sstandard Bchaos/chaosburn Etoggle Xnoboost/nokeybonus/objbonus:10 Glife/sylph/backrow Qfastrom/msgspeedfix -kit:better -kit2:meme -kit3:money -noadamants -smith:alt"
             },
         ]
+    },
+    {
+        groupTitle: "Event Testing",
+        flags: [
+            {
+                title: "50cc",
+                flags: "OA1:quest_cavebahamut/2:quest_traderat/3:quest_ribbonaltar/4:quest_masamunealtar OBrandom:5,tough_quest OCgroup_a:all/group_b:3/do_1:superweapon/do_all:crystal  Kmain/moon/miab:above/nofree/force:magma/start:darkness Pkey Crelaxed/nofree/nogiant/start:cecil,kain,rydia,edward,rosa,yang,palom,porom,cid,edge,fusoya/partner:any/no:cecil,fusoya/j:abilities/nekkie/nodupes/party:3/bye/permajoin/hero Twildish/playable/maxtier:5/unrestrict:moon/miabs:pro Spro/playable/no:apples,sirens Bstandard/alt:gauntlet/whichburn Etoggle/no:sirens Xobjbonus:10/kicheckbonus:2/zonkbonus:2/maxmulti:400/bonuses:mul Gwarp/life/sylph/backrow Qmsgspeedfix -kit:better -kit2:exit -kit3:miab -noadamants -nocursed -spoon -smith:alt -vanilla:miabs"
+            },
+            {
+                title: "100cc",
+                flags: "OA1:quest_monsterqueen/2:quest_monsterking OB1:quest_traderat/2:quest_tradepink OC1:quest_forge/group_a:1/group_b:1 OD1:collect_ki12/2:quest_magma/3:quest_pass/group_c:all/do_all:crystal OE1:collect_boss20/2:quest_giant/group_c:all/do_all:game Kmain/summon/miab:below/nofree/latedark/force:hook/unweighted/start:legend Pchests Crelaxed/nofree/nogiant/j:abilities/nekkie/nodupes/bye Tpro Sstandard/no:apples Bmaybe/nofree/no:waterhag,fabulgauntlet,mirrorcecil,karate,kingqueen/chaosburn Etoggle Xobjbonus:12/kicheckbonus:5/maxmulti:300 Gwarp/life/sylph/backrow Qmsgspeedfix -kit:better -kit2:archer -kit3:exit -noadamants -nocursed -spoon -smith:super"
+            },
+            {
+                title: "150cc",
+                flags: "OA1:boss_odin/2:boss_golbez/random:3,boss/do_1:dkmatter3/do_2:dkmatter3/do_3:dkmatter3/do_4:dkmatter3/do_5:dkmatter3 OB1:quest_forge/2:quest_tradepink/3:quest_baronbasement/random:2,tough_quest/do_1:dkmatter5/do_2:dkmatter5/do_3:dkmatter5/do_4:dkmatter5/do_5:dkmatter5 OC1:quest_falcon/2:quest_murasamealtar/random:2,tough_quest/do_1:dkmatter7/do_2:dkmatter7/do_3:dkmatter7/do_4:dkmatter7 OD1:quest_giant/do_1:dkmatter10 OE1:collect_dkmatter20/2:collect_dkmatter50/do_1:superweapon/do_all:game Kmain/miab:above/char/force:magma/start:earthcrystal Pnone Cstandard/distinct:8/start:any/partner:kicheck/no:fusoya/j:abilities/nekkie/wishes Twildish/playable/maxtier:7/miabs:pro Scabins/free Bchaos/no:fabulgauntlet/chaosburn/whichbez Etoggle/noexp Xnokeybonus/objbonus:10 Glife/backrow Qmsgspeedfix -kit:better -kit2:freedom -kit3:exit -noadamants -spoon -smith:alt,playable -vanilla:miabs"
+            },
+        ]
     }
 ]
 
